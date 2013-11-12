a8e3c24209ce872086677390aa3f896a4ddb1569
Mark Ni <xixian.ni@gmail.com>
2013-11-12 01:30:37 -0800

    moved static files to public folder

--- markni.js
+++ markni.js
@@ -1,4 +1,5 @@
 var connect = require('connect');
 connect.createServer(
-    connect.static(__dirname)
+    connect.static('./public')
 ).listen(8003);
+//TODO: port number should be in config files
