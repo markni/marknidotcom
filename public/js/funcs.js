8b69c0911d6681cb27dc7da93a41552b4ec0ba47
Mark Ni <xixian.ni@gmail.com>
2013-11-15 05:22:20 -0800

    added "appear" animation when dom loaded into view

--- public/js/funcs.js
+++ public/js/funcs.js
@@ -24,6 +24,48 @@ markni.stopAnimations = function(){
     }
 }
 
+
+
+function posY(elm) {
+    var test = elm, top = 0;
+
+    while(!!test && test.tagName.toLowerCase() !== "body") {
+        top += test.offsetTop;
+        test = test.offsetParent;
+    }
+
+    return top;
+}
+
+function viewPortHeight() {
+    var de = document.documentElement;
+
+    if(!!window.innerWidth)
+    { return window.innerHeight; }
+    else if( de && !isNaN(de.clientHeight) )
+    { return de.clientHeight; }
+
+    return 0;
+}
+
+function scrollY() {
+    if( window.pageYOffset ) { return window.pageYOffset; }
+    return Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+}
+
+function checkvisible( elm ) {
+    var vpH = viewPortHeight(), // Viewport Height
+        st = scrollY(), // Scroll Top
+        y = posY(elm);
+
+    console.log(y,vpH,st) ;
+    return (y < (vpH + st));
+}
+
+
+
+
+
 //t = current time
 //b = start value
 //c = change in value
