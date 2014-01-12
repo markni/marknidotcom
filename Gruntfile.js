389cad66918399792ad11e7a38ee5761de5de360
Mark Ni <xixian.ni@gmail.com>
2014-01-12 08:09:26 -0800

    use grunt-usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,25 +45,30 @@ module.exports = function(grunt) {
                     // includes files within path and its sub-directories
                     {expand: true, src: ['package.json'], dest: 'dist/'} ,
                     {expand: true, src: ['public/**'], dest: 'dist/'},
-                    {expand: true, src: ['markni.js'], dest: 'dist/'},
-                    {expand: true, src: ['public/index_production.html'], dest: 'dist/public/', rename:function(dest,src){
-                        return dest+'index.html';
-                    }}
-
+                    {expand: true, src: ['markni.js'], dest: 'dist/'}
 
                 ]
             }
-        }
+        },
+
+		usemin: {
+			html: ['dist/public/index.html'],
+			options: {
+				assetsDirs: ['dist']
+			}
+		}
 
 
-    });
+
+	});
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-usemin');
 
 
-    grunt.registerTask('default', ['copy','concat', 'uglify']);
+    grunt.registerTask('default', ['copy','concat', 'uglify','usemin']);
 
 };
