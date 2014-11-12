d5af87cc3018d1994ec42461ef274bf81b966c2b
Mark Ni <xixian.ni@gmail.com>
2014-11-12 05:50:21 -0800

    add serveStatic as connect 3.x moved their middlewares

--- markni.js
+++ markni.js
@@ -1,5 +1,7 @@
 var connect = require("connect");
+var serveStatic = require('serve-static');
 
-var app = connect().use(connect.static(__dirname + '/public'));
+var app = connect();
+app.use(serveStatic(__dirname + '/public'));
 
 app.listen(8003);
