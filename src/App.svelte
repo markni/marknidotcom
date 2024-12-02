f57a0f7ff6f63d0a0c9fa80da172e5685adf5518
Mark Ni <xixian.ni@gmail.com>
2024-12-01 23:14:21 -0800

    v2

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Vite + Svelte</h1>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+</main>
+
+<style>
+</style>
